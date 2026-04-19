7ca070a37c862c8c2a1e50e915254a56c0873821
dhanan <dj.vizag94@gmail.com>
2026-04-19 11:38:49 +0530

    Adding files to repo

--- EKS/variables.tf
+++ EKS/variables.tf
@@ -1,5 +1,5 @@
 variable "vpc_cidr" {
-  description = "VPC CIDR"
+  description = "VPC CIDR "
   type        = string
 }
 
@@ -11,4 +11,8 @@ variable "private_subnets" {
 variable "public_subnets" {
   description = "Subnets CIDR"
   type        = list(string)
+}
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
 }
